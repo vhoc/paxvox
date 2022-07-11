091626ba786d58377738b4d12cf42625a1319f96
Víctor Olvera <vhocar@gmail.com>
2022-07-10 18:25:08 -0700

    Controlled radio-box input type

--- src/components/PollField.js
+++ src/components/PollField.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TextField, FormControl, FormLabel, FormControlLabel, Checkbox, RadioGroup, Radio, Slider, Select, MenuItem, InputLabel } from '@mui/material'
 import { Typography } from '@mui/material'
 
@@ -17,6 +17,85 @@ const PollField = ( {
     sliderStep
 } ) => {
 
+    const [ fieldData, setFieldData ] = useState([])
+
+    // The following functions are different strategies to update the fieldData state depending on the input type.
+
+    // Input Type: Text
+    const setDataText = ( event ) => {
+        setFieldData( [
+            { name: name, value: event.target.value }
+        ] )
+    }
+
+    // Input Type: CheckBoxGroup
+    const setDataCheckBoxGroup = ( event, option ) => {
+        if ( event.target.checked ) {
+            setFieldData( current =>
+                current.map( obj =>  {
+                    if ( obj.name === option.name ) {
+                        return { ...obj, value: true }
+                    }
+                    return obj
+                } )
+            )
+        } else {
+            setFieldData( current =>
+                current.map( obj =>  {
+                    if ( obj.name === option.name ) {
+                        return { ...obj, value: false }
+                    }
+                    return obj
+                } )
+            )
+        }
+    }
+
+    // Input Type: RadioGroup
+    const setDataRadioGroup = ( event ) => {
+        //console.dir( event )
+        setFieldData( [ { name: title, value: event.target.defaultValue } ] )
+    }
+
+    // The Input change handler. It calls a different function to update the state depending on the input type
+    const handleChange = ( type, event, option ) => {
+        
+        switch ( type ) {
+            case 'checkbox-group':
+                setDataCheckBoxGroup( event, option )
+                break
+
+            case 'text':
+                setDataText( event )
+                break
+
+            case 'radio-group':
+                setDataRadioGroup( event )
+                break
+
+        }
+        
+
+    }
+
+    useEffect( () => {
+
+        const setStartingValues = async () => {
+            if ( type === 'checkbox-group' ) {
+                setFieldData( checkOptions )
+            }
+
+            if ( type === 'radio-group' ) {
+                setFieldData( [ { name: title, value: 'Muy Satisfecho' } ] )
+                let test = [ { name: title, value: 'Muy Satisfecho' } ]
+                console.log( test[0].value )
+            }
+        }
+
+        setStartingValues().catch( console.error )
+        
+    }, [] )
+
     switch ( type ) {
         case 'text':
             return (
@@ -26,18 +105,26 @@ const PollField = ( {
                         label={ title }
                         variant={ `filled` }
                         required={ required }
+                        onChange={ event => {
+                            handleChange( type, event )
+                        } }
                     />
                 </>
                 
             )
 
-        case 'checkbox-group':
+        case 'checkbox-group':            
             return (
                 <FormControl>
                 {
                     checkOptions.length >= 1 &&
                     checkOptions.map( ( option, index ) => (
-                        <FormControlLabel key={ index } control={ <Checkbox /> } label={ option.name } />
+                        <FormControlLabel
+                            key={ index }
+                            control={ <Checkbox /> }
+                            label={ option.name }
+                            onChange={ event => { handleChange( type, event, option ) } }
+                        />
                     ) )                   
                 }
                 </FormControl>
@@ -47,11 +134,16 @@ const PollField = ( {
             return(
                 <FormControl>
                     <FormLabel id={ inputId }>{ title }</FormLabel>
-                    <RadioGroup>
+                    <RadioGroup value={ fieldData.length >= 1 ? fieldData[0].value : 'Muy satisfecho' } onChange={ event => { handleChange( type, event, title ) } }>
                     {
                         radioOptions.length >= 1 &&
                         radioOptions.map( ( option, index ) => (
-                            <FormControlLabel key={ index } control={ <Radio /> } label={ option.name } />
+                            <FormControlLabel
+                                key={ index }
+                                value={ option.name }
+                                control={ <Radio /> }
+                                label={ option.name }
+                            />
                         ) )
                     }
                     </RadioGroup>
@@ -107,5 +199,7 @@ const PollField = ( {
     }
 
 }
+
+
 
 export default PollField
